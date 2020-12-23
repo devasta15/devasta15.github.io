778c54d78e17dd4e2d876b6b2ceb709fe4da9997
devasta15 <75605136+devasta15@users.noreply.github.com>
2020-12-23 09:45:23 +0700

    init

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
 $(document).ready(() => {
-        setTimeout(() => {
-            $('body').css({
-                overflow: 'auto'
-            });
-            $('.banner').hide();
-        }, 3000);
+    setTimeout(() => {
+        $('body').css({
+            overflow: 'auto'
+        });
+        $('.banner').hide();
+    }, 1000);
+
+    $('.toggle').click(() => {
+        $('.toggle').toggleClass('toggle-light');
+        $('span', '.toggle').toggleClass('fa fa-moon fa fa-sun');
+        if($('.main').css('background-color') == 'rgb(50, 50, 50)')
+        {
+            $('.main').css('background-color', '#FFFFFF')
+            $('.row').css({
+                'background-color': '#FFFFFF',
+                'color': '#323232'
+            })
+        }
+        else
+        {
+            $('.main').css('background-color', '#323232')
+            $('.row').css({
+                'background-color': '#323232',
+                'color': '#FFFFFF'
+            })
+        }
     });
+});
